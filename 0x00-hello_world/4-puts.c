39e2dc747b5ce832361d19e4cc92c555647d6b9f
Cemordi2 <emordichukwuma@gmail.com>
2022-06-30 12:38:25 -0700

    My first C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * return always 0
- * */
+ * return: always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
