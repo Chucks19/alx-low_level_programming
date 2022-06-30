dc6e66c6bcc74c5744ac4df588837c8c66a40106
Cemordi2 <emordichukwuma@gmail.com>
2022-06-30 15:29:11 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float f;
 	/* with sizeof we get the size of the type*/
 	printf("Size of a char: %lu byte(s)\n", sizeof(a));
-        printf("Size of a int: %lu byte(s)\n", sizeof(b));                                                                     
+        printf("Size of an int: %lu byte(s)\n", sizeof(b));                                                                     
         printf("Size of a long int: %lu byte(s)\n", sizeof(l));	
         printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
         printf("Size of a float: %lu byte(s)\n", sizeof(f));
