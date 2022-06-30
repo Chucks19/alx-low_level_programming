fe24d499e15d5899f9114a52f5a4d7723062d984
Cemordi2 <emordichukwuma@gmail.com>
2022-06-30 15:33:45 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - is starting of code
  *
- * return is always 0
+ * return: is always 0
  */
 int main(void)
 {
@@ -13,9 +13,9 @@ int main(void)
 	float f;
 	/* with sizeof we get the size of the type*/
 	printf("Size of a char: %lu byte(s)\n", sizeof(a));
-        printf("Size of an int: %lu byte(s)\n", sizeof(b));                                                                     
-        printf("Size of a long int: %lu byte(s)\n", sizeof(l));	
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-        printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
