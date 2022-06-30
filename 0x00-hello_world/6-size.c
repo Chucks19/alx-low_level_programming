c21b32579a1726cdafe99e92fee1b111ac0ff3ca
Cemordi2 <emordichukwuma@gmail.com>
2022-06-30 13:37:43 -0700

    size of type on computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,14 @@ int main(void)
 	char b;
 	double d;
 	float f;
+	long int l;
+	long long int ll;
 	/* with sizeof we get the size of the type*/
 	printf("int size is: %lu bytes\n", sizeof(a));
 	printf("char size is: %lu bytes\n", sizeof(b));
 	printf("double size is: %lu bytes\n", sizeof(d));
 	printf("float size is: %lu bytes\n", sizeof(f));
-	return (0);
+	printf("float size is: %lu bytes\n", sizeof(l));                                                                        
+       printf("float size is: %lu bytes\n", sizeof(ll));
+       return (0);
 }
