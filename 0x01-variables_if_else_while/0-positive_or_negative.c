ef049c6f68a554ce3ecc0c5590611bc876086169
Cemordi2 <emordichukwuma@gmail.com>
2022-07-01 13:25:34 -0700

    If esle

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
+/* main - entry point*/
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -9,8 +10,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	/* your code goes there */
+/* your code goes there */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
