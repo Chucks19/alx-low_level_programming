77ce6920f8058b55b3cbb4fcb809198475c83e76
Cemordi2 <emordichukwuma@gmail.com>
2022-07-01 01:45:41 -0700

    print without printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.io>
+#include <std.io>
 /**
  *
  * Return: always 0
  */
 int main(void)
 {
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-       	write(2, quote, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
