8be81699b0c8e8c5014aa691d75f0c09fc9109e7
Cemordi2 <emordichukwuma@gmail.com>
2022-07-02 22:00:35 +0100

    modulus

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,8 +11,9 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; (c <= 'z' && c != 'q' && c != 'e'); c++)
+	for (c = 'a'; (c <= 'z'); c++)
 	{
+		if (c != 'q' && c!= 'e')
 		putchar(c);
 	}
 		putchar('\n');
