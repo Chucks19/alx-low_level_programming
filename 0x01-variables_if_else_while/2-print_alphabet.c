cf9c2736be5015a843c064c8e4cd91e94b313b40
Cemordi2 <emordichukwuma@gmail.com>
2022-07-02 21:26:23 +0100

    modulus

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * betty style doc for function main goes there
+ *
+ * Return:Alway 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = "c"; c <+= "z"; c++)
+	{
+		putchar(c);
+		putchar("\n");
+	}
+		return (0);
+}
