5362480501aa990339c0b9f5afde411c3082e089
Cemordi2 <emordichukwuma@gmail.com>
2022-07-03 11:51:50 +0100

    modulus

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * betty style doc for function main goes there
+ *
+ * Return:Alway 0
+ */
+int main(void)
+{
+	int c;
+	int d;
+	for (c = 0; (c <= 8); c++)
+	{
+		putchar(c + '0');
+		for (d = 0; d <= 9; d++)
+		{
+			if (d != c)
+			putchar(d + '0');
+		}
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
