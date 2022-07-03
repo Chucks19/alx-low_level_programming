88de4a5c5ec0b0e711297c06c66499bbd87c86c0
Cemordi2 <emordichukwuma@gmail.com>
2022-07-03 13:52:38 +0100

    modulus

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,13 @@ int main(void)
 
 	for (c = '0'; (c <= '8'); c++)
 	{
-		for (d = '0'; d <='9'; d++)
+		for (d = c; d <='9'; d++)
 		{
 		if (c != d)
 		{
 			putchar(c);
 			putchar(d);
+			putchar(',');
 		}
 		}
 	}
