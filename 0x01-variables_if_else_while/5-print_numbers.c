a0a80c1437fcff337b83102fee4b9329c11a588b
Cemordi2 <emordichukwuma@gmail.com>
2022-07-03 10:21:53 +0100

    modulus

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * betty style doc for function main goes there
+ *
+ * Return:Alway 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; (c < 10); c++)
+	{
+		print(c);
+	}
+		print("\n");
+		return (0);
+}
