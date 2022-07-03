c4fbccc4752810601da59a03b7792a2b1b5a19a4
Cemordi2 <emordichukwuma@gmail.com>
2022-07-03 14:39:08 +0100

    modulus

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,9 +18,9 @@ int main(void)
 		{
 			for (e = d; e <=9; e++)
 			{
-				if (e != d && c !=d)
+				if (e != d && c != d)
 				{
-					putchar(e + '0');
+					putchar(c + '0');
 					putchar(d + '0');
 					putchar(e + '0');
 					if (c + d + e != 24)
