e237af4079e06c694bc609119c39ed978b8d265b
Cemordi2 <emordichukwuma@gmail.com>
2022-07-06 06:37:47 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 1; j <= 10; j = j++)
+	{
+		for (i = 97; i <= 122; i = i + 1)
+		{
+			_putchar(i);
+		}
+	_putchar('\n');
+	}
+	return (0);
+}
