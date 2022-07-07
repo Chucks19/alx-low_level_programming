5000efa54b42f0202687c5e2a275b4ed13b7da7e
Cemordi2 <emordichukwuma@gmail.com>
2022-07-07 11:17:51 +0100

    Using _putchar

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,24 +14,29 @@ void times_table(void)
 		for (k = 0; k <= 9; k++)
 		{
 			opp = j * k;
-			if (opp <= 9)
+			if (opp <= 9 && k != 9)
 			{
-				_putchar(opp +48);
+				_putchar(opp + 48);
 				_putchar(44);
 				_putchar(32);
 				_putchar(32);
 			}
+			else if (opp <= 9 && k == 9)
+			{
+				_putchar(opp + 48);
+				_putchar(32);
+			}
 			else if (k != 9)
 			{
-				_putchar((opp/10) +48);
-				_putchar((opp%10) +48);
+				_putchar((opp / 10) + 48);
+				_putchar((opp % 10) + 48);
 				_putchar(44);
 				_putchar(32);
 			}
 			else
 			{
-				_putchar((opp/10) +48);
-				_putchar((opp%10) +48);
+				_putchar((opp / 10) + 48);
+				_putchar((opp % 10) + 48);
 				_putchar(32);
 			}
 		}
