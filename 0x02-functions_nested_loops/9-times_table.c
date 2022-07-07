854fed9f34c7648b3fc8b1fe2f7cb3cc910b01f5
Cemordi2 <emordichukwuma@gmail.com>
2022-07-07 10:33:12 +0100

    Using _putchar

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int j, k, opp;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			opp = j * k;
+			if (opp <= 9)
+			{
+				_putchar(opp +48);
+			}
+			else
+			{
+				_putchar((opp/10) +48);
+				_putchar((opp%10) +48);
+			}
+			_putchar(44);
+			_putchar(32);
+		}
+		_putchar('\n');
+	}
+}
