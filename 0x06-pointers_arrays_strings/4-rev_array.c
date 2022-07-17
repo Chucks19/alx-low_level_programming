63125ef173d964c1a370cb5445a740dfcedf7d08
Cemordi2 <emordichukwuma@gmail.com>
2022-07-17 18:57:14 +0100

    more pointers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array -> revserve the content of an array
+ * @a: array a
+ * @n: element of array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, k;
+
+	n = n - 1;
+	while (i < n)
+	{
+		k = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = k;
+		i++;
+		n--;
+	}
+}
