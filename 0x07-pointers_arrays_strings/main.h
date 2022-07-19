1fda71e55972b836495dc14360433825f739acda
Cemordi2 <emordichukwuma@gmail.com>
2022-07-19 20:30:06 +0100

    much more pointers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,31 +1,3 @@
-
-Skip to content
-Pulls
-Issues
-Marketplace
-Explore
-@Chucks19
-saadrama /
-alx-low_level_programming
-Public
-
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-
-    Insights
-
-alx-low_level_programming/0x07-pointers_arrays_strings/main.h
-@saadrama
-saadrama Tasks
-Latest commit c667cd4 22 hours ago
-History
-1 contributor
-20 lines (20 sloc) 614 Bytes
 #ifndef MAIN_H
 #define MAIN_H
 /**
@@ -46,20 +18,3 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 #endif
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-
-    Terms
-    Privacy
-    Security
-    Status
-    Docs
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
-
-You have no unread notifications
