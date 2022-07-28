59473e98db9bc09d8f33a7873491bbf0c53a38d7
Cemordi2 <emordichukwuma@gmail.com>
2022-07-28 16:23:42 +0100

    More on Malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * malloc_checked - function to check
+ * @b: atring we need to duplicate
+ * Return: A pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *c;
+	c = malloc(sizeof(char) * b);
+	if (c == 0)
+		exit(98);
+	return (c);
+}
