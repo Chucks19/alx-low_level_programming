c1bad68a9978202dc24562ae3239035a7bf5e7b1
Cemordi2 <emordichukwuma@gmail.com>
2022-08-02 03:15:17 +0100

    Create a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+*typedef struct dog - struct for dog
+* @name: name
+ * @age: age
+ * @owner:  owner
+ * Description: This struct is for dog
+ */
+
+struct dog{
+    char* name; 
+    float age;
+    char* owner;
+};
+
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
