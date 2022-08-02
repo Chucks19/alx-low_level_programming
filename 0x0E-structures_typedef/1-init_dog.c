b7383810e526d6db40c3f3a19d2c38c430ef43e7
Cemordi2 <emordichukwuma@gmail.com>
2022-08-02 03:30:47 +0100

    initialise struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * struct dog - check the code
+ * @name: gy
+ * @age: yy
+ * @owner: yu
+ * Description: hggg
+ *
+ * Return: Always 0.
+ */
+
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+
+}
