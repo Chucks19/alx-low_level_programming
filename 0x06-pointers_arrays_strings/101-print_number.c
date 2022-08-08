ceade128ccc12710522e8912f584368597212cc3
Cemordi2 <emordichukwuma@gmail.com>
2022-08-08 16:44:56 +0100

    print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_number - print all
+ *
+ * @n: integer
+ *
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar ('-');
+		_putchar (n);
+
+	if (n < 9)
+		_putchar (n);
+	if (n > 9)
+	{
+		_putchar (n / 10);
+		print_number (n % 10)
+	}
+
+
+
