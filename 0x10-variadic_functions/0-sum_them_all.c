0ebcd882d5dc8055ab5bcb7cc035fefceb137d58
Cemordi2 <emordichukwuma@gmail.com>
2022-08-20 17:49:51 +0100

    a function that adds it args

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,14 +11,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum;
+	int i;
 	va_list pta;
 	
 	va_start(pta, n);
 	if (n == 0)
 		return 0;
 	for (i = 0; i < n; i++)
-	{
 		sum += va_arg(pta, int);
-	}
+	va_end(pta);
 	return (sum);
 }
