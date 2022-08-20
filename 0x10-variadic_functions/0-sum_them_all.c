2929914120e8562f958296ee9638be04c40efcfa
Cemordi2 <emordichukwuma@gmail.com>
2022-08-20 17:59:38 +0100

    a function that adds it args

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,12 +1,11 @@
 #include "variadic_functions.h"
 
 /**
- *  * sum_them_all - the sum of all its parameters.
- *   *
- *    * @n: number of parameters
- *     *
- *      * Return: sum of all all number
- *       */
+ * sum_them_all - the sum of all its parameters.
+ * @n: number of parameters
+ *
+ * Return: sum of all all number
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
