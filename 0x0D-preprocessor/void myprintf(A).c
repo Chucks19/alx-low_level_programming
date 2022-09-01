05b904f951af9827e2c77db69252887be4c16292
Cemordi2 <emordichukwuma@gmail.com>
2022-09-01 13:42:05 +0100

    function that print a name

---
+++ 0x0D-preprocessor/void myprintf(A).c
@@ -0,0 +1,20 @@
+void myprintf(A)
+{int i = 0
+    for (i=0; A[i] != '\0'; *A++)
+        while(A[i] != '%')
+    _putchar(A[i]);
+}
+
+
+switch (A)
+{
+case:
+
+d : i = va_list 
+            _putchar(i);
+
+o
+
+
+
+printf("i will be %o year old in %d days %c \n", 5, 6, time)
