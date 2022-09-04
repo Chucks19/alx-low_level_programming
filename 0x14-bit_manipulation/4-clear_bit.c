c2237f58b7a4c457e74624043e816434ea09643c
Cemordi2 <emordichukwuma@gmail.com>
2022-09-04 21:07:15 +0100

    set bit value to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,7 +14,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
        if (index >= (sizeof(unsigned long int) * 8))
               return (-1);
 
-       *n &= (0 << index);
+       *n &= ~(1 << index);
 
        return (1);
 }
