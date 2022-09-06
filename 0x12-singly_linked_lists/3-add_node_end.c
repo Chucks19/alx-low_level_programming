b5c6b6a820a093f27a2c24a7fc0fa32d07d29047
Cemordi2 <emordichukwuma@gmail.com>
2022-09-06 19:57:00 +0100

    update new element address

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -34,7 +34,6 @@ list_t *add_node_end(list_t **head, const char *str)
               while (temp->next != NULL)
                      temp = temp->next;
               temp->next = new;
-              *head = new;
-       }
+      }
        return (*head);
 }
