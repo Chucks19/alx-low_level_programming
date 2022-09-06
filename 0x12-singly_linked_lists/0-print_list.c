2ae708653244ad22f22323925ec6a99cff016c44
Cemordi2 <emordichukwuma@gmail.com>
2022-09-06 19:32:19 +0100

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	int count;
 
-	nelem = 0;
-	while (h != NULL)
+	count = 0;
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		count++;
 	}
-	return (nelem);
+	return (count);
 }
