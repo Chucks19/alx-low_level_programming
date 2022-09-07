29578c989d335a73819d3061f6954b93d7d6c87c
Cemordi2 <emordichukwuma@gmail.com>
2022-09-07 13:15:29 +0100

    rename function that frees list_h

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,7 +19,7 @@ list_t *add_node(list_t **head, const char *str)
 
        new->str = strdup(str);
 
-       while (new[i] != '\0')
+       while (new != '\0')
        {
               count++;
               i++;
