e0d0f13bffa124c692335fe75cf4da348416c8e0
Cemordi2 <emordichukwuma@gmail.com>
2022-09-13 21:20:07 +0100

    writing to stdout

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
        if (wri == -1 || file_to == -1)
        {
               dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-              exit(98);
+              exit(99);
        }
        err_close = close(file_from);
        if (err_close == -1)
