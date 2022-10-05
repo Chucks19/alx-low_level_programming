7ca42fdbe4df51fd19dfd1b83bfd4e850d142d37
Cemordi2 <emordichukwuma@gmail.com>
2022-10-05 01:46:34 -0700

    task 1

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - lower case
+ *
+ * @c: ASCII code
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
